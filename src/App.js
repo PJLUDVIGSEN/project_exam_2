151c4ad976a861a06df2ee5f911f7836e3e085f6
PJLUDVIGSEN <per-jakob@hotmail.com>
2023-11-28 10:27:45 +0100

    baseproject

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
