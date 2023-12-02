f0a93bbad716fdab0e42a13947ec0a86ddfc1c89
PJLUDVIGSEN <per-jakob@hotmail.com>
2023-12-02 05:33:24 +0100

    text-decoration

--- src/pages/Browse.jsx
+++ src/pages/Browse.jsx
@@ -49,7 +49,7 @@ export function Browse() {
         {filteredGames.map((game) => (
             <div className="card m-2 p-2 d-flex flex-column" key={game.id}>
               <img src={game.acf.image.url} className="card-img-top" alt="..." />
-              <div className="card-body d-flex flex-column justify-content-end">
+              <div className="card-body d-flex flex-column">
                 <h5 className="font-syncopate card-title">{game.acf.title}</h5>
               <h2 className="font-crushed card-text">{game.acf.price},-</h2>
               <Link to={`/${game.id}`}>
